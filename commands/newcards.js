76a4b24f1d5038e4b9bc3e798930669439915939
Murerr <rudy.murer.pro@gmail.com>
2020-12-29 15:20:01 +0100

    Adding Description check for DFG cards (#4)

--- commands/newcards.js
+++ commands/newcards.js
@@ -39,12 +39,13 @@ module.exports = {
 						Database.cards.get(element.id).then(result => {
 							if (result === undefined) {
 								const image_uri = element.image_uris && element.image_uris.png ? element.image_uris.png : element.card_faces[0].image_uris.png;
+								const description = element.oracle_text ? element.oracle_text : element.card_faces[0].oracle_text;
 								Database.cards.set(element.id, element.name).then(() => {
 									channel.send(
 										new Discord.RichEmbed()
 											.setColor('#800080')
 											.setTitle(element.name)
-											.setDescription(element.oracle_text)
+											.setDescription(description)
 											.addField('Set', element.set_name, true)
 											.setURL(element.scryfall_uri)
 											.setImage(image_uri)
